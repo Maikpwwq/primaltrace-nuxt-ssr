45c7dcd91cbf5d7ca562b8cc38389422c6d5c2ef
Maikpwwq <mariasf@correo.udistrital.edu.co>
2023-07-07 22:32:52 -0500

    feat: provisionally change imgs

--- components/lc/header/index.vue
+++ components/lc/header/index.vue
@@ -13,36 +13,27 @@ const drawer = ref(null);
           <!-- Logo -->
           <LcLogoWhiteTextLogo />
           <!-- Desktop view Navigation -->
-          <div
-            class="navigation ml-auto d-lg-block"
-            v-bind:class="[isActive ? 'd-block' : '']"
-            @click="isActive = !isActive"
-          >
+          <div class="navigation ml-auto d-lg-block" v-bind:class="[isActive ? 'd-block' : '']"
+            @click="isActive = !isActive">
             <ul class="d-flex navbar-nav">
               <li class="nav-item">
-                <NuxtLink class="nav-link " to="/"> Custom Components </NuxtLink>
+                <NuxtLink class="nav-link " to="/"> Inicio </NuxtLink>
               </li>
               <li class="nav-item">
-                <NuxtLink class="nav-link" to="/basic-components"
-                  >Basic Components</NuxtLink
-                >
+                <NuxtLink class="nav-link" to="/blog">Blog</NuxtLink>
               </li>
               <li class="nav-item d-md-flex d-sm-none">
                 <!-- login-regiter -->
-                <v-btn
-                  class="btn bg-white"
-                  flat
-                >
-                  <NuxtLink to="https://www.primaltrace.com/" target="_blank" class="text-decoration-none text-dark">Prueba Gratuita</NuxtLink>
+                <v-btn class="btn bg-white" flat>
+                  <!-- https://www.primaltrace.com/ -->
+                  <NuxtLink to="https://primaltrace-nuxt-ssr-f3ko.vercel.app/" target="_blank"
+                    class="text-decoration-none text-dark">Prueba Gratuita</NuxtLink>
                 </v-btn>
               </li>
             </ul>
           </div>
-          <v-app-bar-nav-icon
-            width="30"
-            class="d-md-none d-sm-flex ml-auto drawer-icon text-white mr-0"
-            @click.stop="drawer = !drawer"
-          ></v-app-bar-nav-icon>
+          <v-app-bar-nav-icon width="30" class="d-md-none d-sm-flex ml-auto drawer-icon text-white mr-0"
+            @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
         </v-toolbar>
       </v-container>
     </v-app-bar>
@@ -54,17 +45,17 @@ const drawer = ref(null);
   <v-navigation-drawer v-model="drawer" temporary>
     <ul class="navbar-nav py-4 ">
       <li class="nav-item mb-3">
-        <NuxtLink class="nav-link text-dark py-0" to="/"> Custom Components </NuxtLink>
+        <NuxtLink class="nav-link text-dark py-0" to="/"> Inicio </NuxtLink>
       </li>
       <li class="nav-item mb-4">
-        <NuxtLink class="nav-link text-dark py-0" to="/basic-components"
-          >Basic Components</NuxtLink
-        >
+        <NuxtLink class="nav-link text-dark py-0" to="/blog">Blog</NuxtLink>
       </li>
       <li class="nav-item ml-3 mr-3">
         <!-- login-regiter -->
         <v-btn color="error" block flat>
-         <NuxtLink to="https://www.primaltrace.com/" target="_blank" class="text-decoration-none text-white">Prueba Gratuita</NuxtLink>
+          <!-- https://www.primaltrace.com/ -->
+          <NuxtLink to="https://primaltrace-nuxt-ssr-f3ko.vercel.app/" class="text-decoration-none text-white">Prueba
+            Gratuita</NuxtLink>
         </v-btn>
       </li>
     </ul>
