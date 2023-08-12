ec3aede2243e3d2d542d4740633f7ec8bc51de4f
Maikpwwq <mariasf@correo.udistrital.edu.co>
2023-08-12 18:41:39 -0500

    fix...

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -28,7 +28,7 @@ export default defineNuxtConfig({
       apiKeyTatum: `${process.env.VITE_API_KEY_TATUM}` || "",
       apiKeyThirdWeb: `${process.env.VITE_THIRDWEB_PRIVATE_KEY}` || "",
       clientIdThirdWeb: `${process.env.VITE_THIRDWEB_CLIENT_ID}` || "",
-      web3AuthClientID: `${import.meta.env..VITE_WEB3AUTH_CLIENT_ID}` || `${process.env.VITE_WEB3AUTH_CLIENT_ID}` || "",
+      web3AuthClientID: "BPeTrfVgPsmhRzxk4Js7mMwdQuCm2nrpu0It9HoZ5-f8e-6Y6Nx6qBINcN45q8lzrvTVt2vIKpclcE8OFld8BWU", // `${import.meta.env..VITE_WEB3AUTH_CLIENT_ID}` || `${process.env.VITE_WEB3AUTH_CLIENT_ID}` ||
     },
   },
   // Nuxt has built-in support for loading .env files. Avoid directly importing it from nuxt.config.
