b932bc8f3900aebea011344db3fa9729372e918c
Maikpwwq <mariasf@correo.udistrital.edu.co>
2023-08-23 22:55:49 -0500

    feat: get catalog

--- components/section/read-contract/ReadSmartContract.vue
+++ components/section/read-contract/ReadSmartContract.vue
@@ -1,11 +1,11 @@
 <script setup lang="ts">
-// import GetCatalog from "@/components/section/select-smart-contract/GetCatalog.vue";
+import GetCatalog from "@/components/section/select-smart-contract/GetCatalog.vue";
 import GetProduct from "@/components/section/add-product/GetProduct.vue";
 import GetTraceabilityInfo from "@/components/section/add-traceability-info/GetTraceabilityInfo.vue";
 </script>
 <template>
   <div id="readContract" >
-    <!-- <GetCatalog/> -->
+    <GetCatalog/>
     <GetProduct />
     <GetTraceabilityInfo />
   </div>
