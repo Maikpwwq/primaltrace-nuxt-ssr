d3a86a7d6aab1b2c7ca0bf04e51118eb6f0618f3
Maikpwwq <mariasf@correo.udistrital.edu.co>
2023-08-23 22:43:46 -0500

    fix: private sdk disable secretKey

--- services/thridWeb/sdkPrivateInstance.js
+++ services/thridWeb/sdkPrivateInstance.js
@@ -2,7 +2,7 @@ import { ThirdwebSDK } from "@thirdweb-dev/sdk";
 import { PolygonZkevmTestnet } from "@thirdweb-dev/chains";
 
 const config = useRuntimeConfig();
-const THIRDWEB_PRIVATE_KEY = config.public.thirdwebPrivateKey;
+// const THIRDWEB_PRIVATE_KEY = config.public.thirdwebPrivateKey;
 const THIRDWEB_CLIENT_ID = config.public.thirdwebClientId;
 const PRIVATE_KEY = config.public.personalAccountPrivateKey;
 
@@ -11,7 +11,7 @@ const sdk = ThirdwebSDK.fromPrivateKey(
   PolygonZkevmTestnet, // "ethereum",
   {
     clientId: THIRDWEB_CLIENT_ID, // Use client id if using on the client side, get it from dashboard settings
-    secretKey: THIRDWEB_PRIVATE_KEY, // Use secret key if using on the server, get it from dashboard settings
+    // secretKey: THIRDWEB_PRIVATE_KEY, // Use secret key if using on the server, get it from dashboard settings
   },
 );
 
